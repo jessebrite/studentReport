ea21ca16d393d6f7d1dd45cd36bee9e9243852f3
charliescode <charles.gold@turntabl.io>
2021-10-11 16:06:15 +0000

    added the testfile for LectureTest and NaughtyStudentTest

---
+++ test/com/company/LectureTest.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class LectureTest {
+
+    //@Test
+//    public void addStudentsToEnter(){
+//        Lecture lecture = new Lecture();
+//
+//        //given
+//        List<Double> student1 = new ArrayList<>();
+//        student1.add(80.0);
+//
+//        List<Double> student2 = new ArrayList<>();
+//        student2.add(30.0);
+//
+//        List<Double> student3 = new ArrayList<>();
+//        student3.add(40.0);
+//
+//        //when
+//        lecture.enter(new Student(student1));
+//        lecture.enter(new Student(student2));
+//        lecture.enter(new Student(student3));
+//
+//
+//        //then
+//        assertEquals(80,lecture.getHighestAverage());
+
+    //}
+}
---
+++ test/com/company/NaughtyStudentTest.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NaughtyStudentTest {
+//    @Test
+//    public void test_10_percent_increment(){
+//        //given
+//        List<Double> list = new ArrayList<>();
+//        list.add(20.0);
+//        list.add(20.0);
+//        list.add(20.0);
+//
+//        //when
+//        NaughtyStudent naughtyStudent = new NaughtyStudent(list);
+//
+//        //then
+//        assertEquals(22.0,naughtyStudent.increaseAverageGrade());
+//    }
+
+}
